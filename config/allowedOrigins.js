b1f998c3f67c24c5d5639094bfa4c001ea3af48a
rob662002 <robertdowney662002@gmail.com>
2023-06-18 08:14:00 +0530

    just checking

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
+    'https://mitshalls.onrender.com',
     'http://localhost:3000'
 ];
 
